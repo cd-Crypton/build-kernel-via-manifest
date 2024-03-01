19a0882ab7a005d4e8fc583904557310ab52459a
Carlo Dandan <carlodandan.personal@proton.me>
2024-03-01 04:32:26 +0000

    kernel_workspace: Added AnyKernel3 and switch upload target.
    Signed-off-by: Carlo Dandan <carlodandan.personal@proton.me>

--- workspace/upload.sh
+++ workspace/upload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl --upload-file android-kernel/out/android-4.19-stable/dist/Image.gz https://transfer.sh/Image.gz > download.txt
+curl --upload-file android-kernel/AnyKernel3/UPDATE-AnyKernel3.zip https://transfer.sh/UPDATE-AnyKernel3.zip > download.txt
