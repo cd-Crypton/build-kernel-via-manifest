a1625e2bd1d7c341385e484609c82991a22b5a43
Carlo Dandan <carlodandan.personal@proton.me>
2024-03-01 09:45:09 +0800

    kernel_workspace: Added build script.

---
+++ workspace/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Reinstall packages just to make sure, no harm anyway
+sudo apt-get install bison flex rsync -y
+
+# Check if 'repo' command exists in PATH
+if ! command -v repo &> /dev/null; then
+    echo "'repo' command not found. Installing..."
+    mkdir -p ~/bin  # Create bin directory if it doesn't exist
+    curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo  # Download 'repo' tool
+    chmod a+x ~/bin/repo  # Make 'repo' executable
+    echo "'repo' command installed successfully."
+else
+    echo "'repo' command already exists."
+fi
+
+# Build Kernel
+BUILD_CONFIG=build.config build/build.sh
