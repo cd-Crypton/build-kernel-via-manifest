82aa2dc118887fa3f6c0977e947dbf79b2c82e2b
Carlo Dee <carlodee.official@proton.me>
2024-04-08 23:15:05 +0800

    kernel_workspace: Use same method used in build.sh for detecting repo.
    Signed-off-by: Carlo Dee <carlodee.official@proton.me>

--- workspace/reposync.sh
+++ workspace/reposync.sh
@@ -3,11 +3,16 @@
 # Make sure to always install these packages to avoid build error abour rsycn, bison and flex
 sudo apt-get install bison flex rsync bison device-tree-compiler bc -y
 
-# Make sure repo is installed
-mkdir -p ~/bin
-curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
-chmod a+x ~/bin/repo
-sudo ln -sf ~/bin/repo /usr/bin/repo
+# Check if 'repo' command exists in PATH
+if ! command -v repo &> /dev/null; then
+    echo "'repo' command not found. Installing..."
+    mkdir -p ~/bin  # Create bin directory if it doesn't exist
+    curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo  # Download 'repo' tool
+    chmod a+x ~/bin/repo  # Make 'repo' executable
+    echo "'repo' command installed successfully."
+else
+    echo "'repo' command already exists."
+fi
 
 # Create directory for repo sync
 mkdir -p android-kernel
